8ca45c0f5fcbd38a863e718bf7c29554d1de0082
gloxoss <112545439+gloxoss@users.noreply.github.com>
2023-05-24 22:34:14 +0100

    Create _puts.c

---
+++ _puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * main - code entry point
+  * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	getchar();
+	return (0);
+}

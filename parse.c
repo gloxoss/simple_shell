3986b698ecb60ba923f0516e3717e2ff6ab4e4bf
gloxoss <112545439+gloxoss@users.noreply.github.com>
2023-05-24 23:53:59 +0100

    Create parse.c

---
+++ parse.c
@@ -0,0 +1,24 @@
+/**
+ * parse -  parses the line into commands and arguments
+ *@line : command line
+ * Return :  **char
+ */
+char **parse(char *line)
+{
+char **arguments = malloc(sizeof(char *) * (strlen(line) / 2 + 1));
+if (arguments == NULL)
+{
+return (NULL);
+}
+
+int i = 0;
+char *command = strtok(line, " ");
+while (command != NULL)
+{
+arguments[i] = command;
+command = strtok(NULL, " ");
+i++;
+}
+
+return (arguments);
+}

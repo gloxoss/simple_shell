2a48fd9536343fd9770d4bcb2d6acf588eb3e0a9
gloxoss <112545439+gloxoss@users.noreply.github.com>
2023-05-25 00:12:05 +0100

    Update _strcpy.c

--- _strcpy.c
+++ _strcpy.c
@@ -1,4 +1,12 @@
 #include "main.h"
+
+/**
+  * _strcpy - copys str to another
+  * @dest: destination string
+  * @src: the str that we gonna copy
+  *
+  * Return: the pointer to dst
+  */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;

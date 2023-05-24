52c3b4c97e2a18dc12c64ae76a2352d7a344d952
gloxoss <112545439+gloxoss@users.noreply.github.com>
2023-05-24 22:39:41 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,17 @@
 
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define MAX_COMMAND_LINE_LEN 1024
+#define MAX_COMMAND_LINE_ARGS 128

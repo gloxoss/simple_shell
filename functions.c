72147a61336222335ce7cddd915a240e93fffc9d
gloxoss <112545439+gloxoss@users.noreply.github.com>
2023-05-24 23:41:44 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -28,20 +28,17 @@ exit(0);
 void execute(char *command, char **arguments)
 {
 pid_t pid = fork();
-
 if (pid == 0)
 {
 char *path = getenv("PATH");
 char *full_path = NULL;
 size_t len = strlen(command);
-
 /* Split the PATH environment variable into an array of strings.*/
 char **paths = malloc(sizeof(char *) * (strlen(path) / 2 + 1));
 if (paths == NULL)
 {
 return;
 }
-
 int i = 0;
 char *token = strtok(path, ":");
 while (token != NULL)
@@ -49,9 +46,7 @@ while (token != NULL)
 paths[i] = token;
 token = strtok(NULL, ":");
 i++;
-}
-
-    /* Iterate through the array of paths and try to find the command.*/
+}/* Iterate through the array of paths and try to find the command.*/
 for (i = 0; paths[i] != NULL; i++)
 {
 size_t path_len = strlen(paths[i]);
@@ -60,11 +55,9 @@ if (full_path_temp == NULL)
 {
 return;
 }
-
 strcpy(full_path_temp, paths[i]);
 strcat(full_path_temp, "/");
 strcat(full_path_temp, command);
-
 /* Check if the command exists in the current path.*/
 int status = access(full_path_temp, F_OK);
 if (status == 0)
@@ -72,20 +65,16 @@ if (status == 0)
 full_path = full_path_temp;
 break;
 }
-
 free(full_path_temp);
 }
-
 /* Free the array of paths.*/
 free(paths);
-
 /* Execute the command.*/
 if (full_path == NULL)
 {
 write(2, "Command not found: %s\n", command);
 exit(1);
 }
-
 int status = execvp(full_path, arguments);
 if (status == -1)
 {
@@ -93,14 +82,12 @@ perror(command);
 exit(1);
 }
 }
-
 /* If the child process was not created successfully, wait for it to exit.*/
 else if (pid > 0)
 {
 waitpid(pid, &status, 0);
 }
 }
-
 /**
  * strip - strips the newline character from the end of the line that was read
  *@str : string

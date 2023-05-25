9036fbd39876ed8190c83dc3283009da156e02a5
gloxoss <112545439+gloxoss@users.noreply.github.com>
2023-05-25 03:39:12 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+int main(int argc, char **argv, char **envp) {
+  int fd;
+
+  if (argc == 2) {
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+      if (errno == EACCES) {
+        exit(126);
+      } else if (errno == ENOENT) {
+        exit(127);
+      } else {
+        return EXIT_FAILURE;
+      }
+    }
+  } else {
+    fd = STDIN_FILENO;
+  }
+
+  setup_signal_handler();
+
+  prompt(argv, envp);
+
+  if (fd != STDIN_FILENO) {
+    close(fd);
+  }
+
+  return 0;
+}
+

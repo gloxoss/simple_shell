70143e95022a4dc765f8581bdffbdcb2a5a8b718
gloxoss <112545439+gloxoss@users.noreply.github.com>
2023-05-25 03:48:32 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,13 @@
 #include "main.c"
 
-int main(int argc, char **argv, char **envp) {
-  int fd;
+int main(void) {
+   signal(SIGINT, handle_ctrl_c);
 
-  if (argc == 2) {
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-      if (errno == EACCES) {
-        exit(126);
-      } else if (errno == ENOENT) {
-        exit(127);
-      } else {
-        return EXIT_FAILURE;
-      }
+    while (1)
+    {
+        prompt();
     }
-  } else {
-    fd = STDIN_FILENO;
-  }
 
-  setup_signal_handler();
-
-  prompt(argv, envp);
-
-  if (fd != STDIN_FILENO) {
-    close(fd);
-  }
-
-  return 0;
+    return 0;
 }
 
